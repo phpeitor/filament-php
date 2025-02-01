e5c3c6b7efdbed2d1c79e3355d58e7597028d684
FORTEL\alejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-01-31 20:00:26 -0500

    filamenrt overview commit

--- app/Filament/Resources/CustomerResource/Widgets/CustomerOverview.php
+++ app/Filament/Resources/CustomerResource/Widgets/CustomerOverview.php
@@ -10,7 +10,7 @@ class CustomerOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Unique views', '192.1k')
+            Stat::make('Unique views', '200.1k')
                 ->description('32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success')
