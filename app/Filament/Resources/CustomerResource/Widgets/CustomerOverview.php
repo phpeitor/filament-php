4e65b0206738bbd4c0fe8f968294320914046125
FORTEL\alejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-03-04 16:46:04 -0500

    widget commit

--- app/Filament/Resources/CustomerResource/Widgets/CustomerOverview.php
+++ app/Filament/Resources/CustomerResource/Widgets/CustomerOverview.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Resources\CustomerResource\Widgets;
 
+use App\Models\User;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class CustomerOverview extends BaseWidget
@@ -10,10 +13,23 @@ class CustomerOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Unique views', '200.1k')
-                ->description('32k increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
+            Stat::make('Usuarios', User::count())
+                ->description('32% increase')
+                ->descriptionIcon('heroicon-m-user-group', IconPosition::Before)
+                ->chart([2,3,5,10,20,40])
+                ->color('success'),
+
+            Card::make('Total', 100)
+                ->color('primary')
+                ->description('Total users'),
+
+            Card::make('Admin', 4)
+                ->color('danger')
+                ->description('Admin users'),
+
+            Card::make('Active', 88)
                 ->color('success')
+                ->description('Active users'),
         ];
     }
 }
--- app/Providers/Filament/PhpeitorPanelProvider.php
+++ app/Providers/Filament/PhpeitorPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\CustomerResource\Widgets\CustomerOverview;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -55,14 +56,15 @@ class PhpeitorPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
+            ->plugins([
+                SystemInfoPlugin::make()
+                    //->setSort(3),
+            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
+                CustomerOverview::class,
                 //Widgets\FilamentInfoWidget::class,
-            ])
-            ->plugins([
-                SystemInfoPlugin::make()
-                    ->setSort(2),
             ])
             ->middleware([
                 EncryptCookies::class,
